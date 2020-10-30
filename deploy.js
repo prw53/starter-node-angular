39d2c9929dcfc2f93cacdba3018a62927cef9eef
Peter <peter.waller53@gmail.com>
2020-10-30 18:21:52 +0000

    updated node-ssh version to 10.0.0

--- deploy.js
+++ deploy.js
@@ -86,7 +86,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '35.170.242.9',
+      host: '3.83.137.39',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Installing 'node-cmd'"
 npm install node-cmd
-echo "Installing 'node-ssh'"
-npm install node-ssh
+echo "Installing 'node-ssh@10.0.0'"
+npm install node-ssh@10.0.0
 echo "Running 'deploy.js'"
 node deploy.js
